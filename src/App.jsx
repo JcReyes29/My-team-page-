95a024fd99366b0d020266a0a325e9eeeb276459
JcReyes29 <j.creyesp29@gmail.com>
2024-10-01 19:36:36 -0500

    My first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Images from './components/images'
+
+function App() {
+
+  return (
+    <>
+      <div className='container w-screen p-5 gap-10'>
+        <header className='title'>
+          <h1 className=' text-3xl text-start font-semibold'>The creative crew</h1>
+        </header>
+        <section className=' text-start flex flex-col gap-3'>
+          <h2 className='font-semibold'>WHO WE ARE</h2>
+          <p>We are team of creatively diverse, driven, innovative individuals working in various locations from the world.</p>
+        </section>
+        <Images></Images>
+
+      </div>
+    </>
+  )
+}
+
+export default App
