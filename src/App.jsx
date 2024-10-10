78f2b71e420668dae9f44688de6c2727af003b37
JcReyes29 <j.creyesp29@gmail.com>
2024-10-10 18:51:17 -0500

    photos update 2.5

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div className='container w-screen p-5 gap-10'>
+      <div className='container w-screen p-5 gap-10 lg:mx-auto'>
         <header className='title'>
           <h1 className=' text-3xl text-start font-semibold'>The creative crew</h1>
         </header>
--- src/components/pictures/Images.jsx
+++ src/components/pictures/Images.jsx
@@ -1,59 +1,60 @@
 import './Images.css'
+import photo1 from '/public/photos/photo1.png'
+import photo2 from '/public/photos/photo2.png'
+import photo3 from '/public/photos/photo3.png'
+import photo4 from '/public/photos/photo4.png'
+import photo5 from '/public/photos/photo5.png'
+import photo6 from '/public/photos/photo6.png'
 
-function Images() {
+export default function Images() {
     return (
         <>
             <div className='container'>
                 <div className="card flex-col">
                     <div className='flex card_menu'>
-                        <img src="/public/photos/photo1.png" width={140}></img>
+                        <img src={photo1} width={140} />
                         <h2 className="  [writing-mode:vertical-rl] text-xs artist">PRODUCT OWNER</h2>
                     </div>
                     <h3 className="font-semibold name_artist">Bill Mahoney</h3>
                 </div>
                 <div className="card flex-col">
                     <div className='flex card_menu'>
-                        <img src="/public/photos/photo2.png" width={140}></img>
+                        <img src={photo2} width={140} />
                         <h2 className="  [writing-mode:vertical-rl] text-xs artist">ART DIRECTOR</h2>
                     </div>
                     <h3 className="font-semibold name_artist">Saba Cabrera</h3>
                 </div>
                 <div className="card flex-col">
                     <div className='flex card_menu'>
-                        <img src="/public/photos/photo3.png" width={140}></img>
+                        <img src={photo3} width={140} />
                         <h2 className="  [writing-mode:vertical-rl] text-xs artist">TECH LEAD</h2>
                     </div>
                     <h3 className="font-semibold name_artist">Shae Le</h3>
                 </div>
                 <div className="card flex-col">
                     <div className='flex card_menu'>
-                        <img src="/public/photos/photo4.png" width={140}></img>
+                        <img src={photo4} width={140} />
                         <h2 className="  [writing-mode:vertical-rl] text-xs artist">UX DESIGNER</h2>
                     </div>
                     <h3 className="font-semibold name_artist">Skylah Lu</h3>
                 </div>
                 <div className="card flex-col">
                     <div className='flex card_menu'>
-                        <img src="/public/photos/photo5.png" width={140}></img>
+                        <img src={photo5} width={140} />
                         <h2 className="  [writing-mode:vertical-rl] text-xs artist">DEVELOPER</h2>
                     </div>
                     <h3 className="font-semibold name_artist">Griff Richards</h3>
                 </div>
                 <div className="card flex-col">
                     <div className='flex card_menu'>
-                        <img src="/public/photos/photo6.png" width={140}></img>
+                        <img src={photo6} width={140} />
                         <h2 className="  [writing-mode:vertical-rl] text-xs artist">DEVELOPER</h2>
                     </div>
                     <h3 className="font-semibold name_artist">Stan John</h3>
                 </div>
 
             </div>
-
-
-
         </>
 
     )
 }
-
-export default Images
